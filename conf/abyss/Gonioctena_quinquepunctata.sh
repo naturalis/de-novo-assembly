b7e15e64ff274e67c588f60eabd3196bf60ca80e
Rutger Vos <rutgeraldo@gmail.com>
2013-12-24 11:33:38 +0100

    Create Gonioctena_quinquepunctata.sh

---
+++ conf/abyss/Gonioctena_quinquepunctata.sh
@@ -0,0 +1,14 @@
+# environment variables to be sourced by abyss shell script
+
+DATA=/home/assembly/data/schilthuizen/RawData
+PAIRS="pair1 pair2 pair3 pair4 pair5 pair6"
+
+# individual 1, host plant 1
+pair1="$DATA/C0A7AACXX_101851-07_CAGATC_L001_R1.fastq.gz $DATA/C0A7AACXX_101851-07_CAGATC_L001_R2.fastq.gz"
+pair2="$DATA/C0A7AACXX_101851-07_CAGATC_L002_R1.fastq.gz $DATA/C0A7AACXX_101851-07_CAGATC_L002_R2.fastq.gz"
+pair3="$DATA/D0TRBACXX_101851-07_CAGATC_L004_R1.fastq.gz $DATA/D0TRBACXX_101851-07_CAGATC_L004_R2.fastq.gz"
+
+# individual 2, host plant 2
+pair4="$DATA/C0A7AACXX_101851-10_CTTGTA_L001_R1.fastq.gz $DATA/C0A7AACXX_101851-10_CTTGTA_L001_R2.fastq.gz"
+pair5="$DATA/C0A7AACXX_101851-10_CTTGTA_L002_R1.fastq.gz $DATA/C0A7AACXX_101851-10_CTTGTA_L002_R2.fastq.gz"
+pair6="$DATA/D0TRBACXX_101851-10_CTTGTA_L004_R1.fastq.gz $DATA/D0TRBACXX_101851-10_CTTGTA_L004_R2.fastq.gz"
