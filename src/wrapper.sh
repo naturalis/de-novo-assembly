febdbdcf28fdc7e6d46948f5661666fc34342d56
rvosa <rutgeraldo@gmail.com>
2014-01-06 16:10:13 +0100

    made more generic

--- src/wrapper.sh
+++ src/wrapper.sh
@@ -3,7 +3,19 @@
 # for the two species sequentially so we can launch one
 # job after the other and each can max out the available
 # resources
+
+# tool is the first command line argument, e.g.
+# "./velvet.sh"
+TOOL=$1
+if [ -x "$TOOL" ]; then
+	echo "going to run $TOOL"
+else
+	echo "no executable $TOOL"
+	exit
+fi
+
+# iterate over species list
 SPECIESLIST="Rhagoletis_cerasi Gonioctena_quinquepunctata"
 for SPECIES in $SPECIESLIST; do
-	./soapdenovo2.sh $SPECIES
+	$TOOL $SPECIES
 done
