54a441c132fcad3524a65457d19f5f54c0b1fb96
rvosa <rutgeraldo@gmail.com>
2014-01-08 12:39:34 +0100

    changed variable name for reference sequence from REFSEQ to REFERENCE, in line with conventions from tomato project

--- conf/bwa/Gonioctena_quinquepunctata.sh
+++ conf/bwa/Gonioctena_quinquepunctata.sh
@@ -1,3 +1,3 @@
 # this is the scaffolded genome for the colorado potato beetle, which is close enough to use as a
 # reference when scanning for variable loci. unfortunately, we have no annotations for this genome.
-REFSEQ=/home/assembly/de-novo-assembly/results/refseqs/i5k/Leptinotarsa_decemlineata/Ldec_FINAL.genome.fa
+REFERENCE=/home/assembly/de-novo-assembly/results/refseqs/i5k/Leptinotarsa_decemlineata/Ldec_FINAL.genome.fa
--- conf/bwa/Rhagoletis_cerasi.sh
+++ conf/bwa/Rhagoletis_cerasi.sh
@@ -1,4 +1,4 @@
 # this is the scaffolded genome of the mediterranean fruit fly, which is close enough
 # to R. cerasi to use as a reference for the use case of scanning for variable loci.
-REFSEQ=/home/assembly/de-novo-assembly/results/refseqs/i5k/Ceratitis_capitata/Ccap01172013-genome.fa
+REFERENCE=/home/assembly/de-novo-assembly/results/refseqs/i5k/Ceratitis_capitata/Ccap01172013-genome.fa
 GFF3=/home/assembly/de-novo-assembly/results/refseqs/i5k/Ceratitis_capitata/CCAP_v0.5.2.models.gff3.gz
