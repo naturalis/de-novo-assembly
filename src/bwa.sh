6a749f441bb409c709a5235fcfa6e2beac67204e
rvosa <rutgeraldo@gmail.com>
2014-01-08 12:39:45 +0100

    updated BWA script

--- src/bwa.sh
+++ src/bwa.sh
@@ -1,37 +1,148 @@
 #!/bin/bash
-#This script needs no commandline input
+# species name is provided on the command line
+SPECIES=$1
 
-#Location of the project. All paths are constructed relative
-#to this location
-PROJECT=/home/carla/Documents/ABySS_Mapping
+# verify command line argument
+if [ "$SPECIES" == "Rhagoletis_cerasi" ]; then
+        echo "going to assemble Rhagoletis cerasi"
+elif [ "$SPECIES" == "Gonioctena_quinquepunctata" ]; then
+        echo "going to assemble Gonioctena quinquepunctata"
+else 
+        echo "Usage: $0 <Rhagoletis_cerasi or Gonioctena_quinquepunctata>"
+        exit
+fi
 
-#No results directory is made, because BWA saves all output files in the
-#directory containing the input
+# location of project. all paths are constructed relative 
+# to this location
+PROJECT=/home/assembly/de-novo-assembly
 
-#Path to the directory containing the input. Made seperate because of 
-#frequently used.
-INPUT=$PROJECT/Input
+# read in the environment variables from the config file
+# containing the location of the reads
+source $PROJECT/conf/abyss/$SPECIES.sh
 
-#Path to the directory containing BWA.
-BWA=/home/carla/Downloads/BWA/bwa-0.7.5a
+# read in the environment variables from the config file
+# containing the location of the reference
+source $PROJECT/conf/bwa/$SPECIES.sh
 
-#Log file from STDOUT for indexing
-LOG=$INPUT/index.log
-#Error messages from STDERR for indexing
-ERR=$INPUT/index.err
+# make output dir
+RESULTS=$PROJECT/results/bwa/$SPECIES
+mkdir -p $RESULTS
 
-#Execute the command for indexing the fasta file. Creating a log file and 
-#an error file.
-/usr/bin/time --verbose $BWA/./bwa index $INPUT/ABySS_Output-contigs.fa  > $LOG > $ERR
+# log file from STDOUT
+LOG=$RESULTS/$SPECIES.log
 
-#Log file from STDOUT for mapping
-LOG2=$INPUT/align.log
-#Error messages from STDERR for mapping
-ERR2=$INPUT/align.err
+# error messages from STDERR
+ERR=$RESULTS/$SPECIES.err
 
-#Execute the command for mapping. Creating a log file and an error file.
-/usr/bin/time --verbose $BWA/./bwa mem -x $INPUT/ABySS_Output-contigs.fa $INPUT/C0A7AACXX_101851-02_TGACCA_L001_R1.fastq.gz $INPUT/C0A7AACXX_101851-02_TGACCA_L001_R2.fastq.gz > aln-pe.sam > $LOG2 > $ERR2
+# number of cores to use
+CORES=8
 
+# recreate BWA index if not exists
+if [ ! -e $REFERENCE.bwt ]; then
+	echo "going to index $REFERENCE" >> $LOG
 
+	# Warning: "-a bwtsw" does not work for short genomes, 
+	# while "-a is" and "-a div" do not work not for long 
+	# genomes. Please choose "-a" according to the length 
+	# of the genome.
+	bwa index -a bwtsw $REFERENCE >> $LOG 2>> $ERR
+else 
+	echo "$REFERENCE already indexed" >> $LOG
+fi
 
+# this will grow into a list of sorted BAM files to merge
+BAMS=""
 
+# iterate over mate pairs
+for PAIR in $PAIRS; do
+	echo "going to process $PAIR"
+
+	# list the FASTQ files in this pair. this should be
+	# two files (paired end)
+	FASTQS=${!PAIR}
+
+	# lists of produced files
+	SAIS=""
+	SAM=""
+
+	# iterate over FASTQ files
+	for FASTQ in $FASTQS; do
+
+		# create new names
+		LOCALFASTA=`echo $REFERENCE | sed -e 's/.*\///'`
+		LOCALFASTQ=`echo $FASTQ | sed -e 's/.*\///'`
+		OUTFILE=$RESULTS/$LOCALFASTQ-$LOCALFASTA.sai
+		SAIS="$SAIS $OUTFILE"
+		SAM=`echo $OUTFILE | sed -e "s/_R.*/-$LOCALFASTA.sam/"`
+
+		# note: we don't do basic QC here, because that might mean
+		# that the mate pairs in the FASTQ files go out of order,
+		# which will result in the bwa sampe step taking an inordinate
+		# amount of time
+
+		# do bwa aln if needed
+		if [ ! -e $OUTFILE ]; then
+			echo "going to align $FASTQ against $REFERENCE" >> $LOG
+
+			# use $CORES threads
+			bwa aln -t $CORES $REFERENCE $FASTQ > $OUTFILE 2>> $ERR
+		else
+			echo "alignment $OUTFILE already created" >> $LOG
+		fi
+	done
+
+	# do bwa sampe if needed
+	if [ ! -e $SAM ]; then
+
+		# create paired-end SAM file
+		echo "going to run bwa sampe $FASTA $SAIS $FASTQS > $SAM" >> $LOG
+		bwa sampe $REFERENCE $SAIS $FASTQS > $SAM 2>> $ERR
+	else
+		echo "sam file $SAM already created" >> $LOG
+	fi		
+
+	# do samtools filter if needed
+	if [ ! -e $SAM.filtered ]; then
+		# -bS   = input is SAM, output is BAM
+		# -F 4  = remove unmapped reads
+		# -q 50 = remove reads with mapping qual < 50
+		# XXX maybe increase -q?
+		echo "going to run samtools view -bS -F 4 -q 50 $SAM > $SAM.filtered" >> $LOG
+		samtools view -bS -F 4 -q 50 $SAM > $SAM.filtered 2>> $ERR
+		gzip -9 $SAM
+	else
+		echo "sam file $SAM.filtered already created" >> $LOG
+	fi
+
+	# do samtools sorting if needed
+	if [ ! -e $SAM.sorted.bam ]; then
+
+		# sorting is needed for indexing
+		echo "going to run samtools sort $SAM.filtered $SAM.sorted" >> $LOG
+		samtools sort $SAM.filtered $SAM.sorted 2>> $ERR
+	else
+		echo "sam file $SAM.sorted already created" >> $LOG
+	fi
+
+	# created index for BAM file if needed
+	if [ ! -e $SAM.sorted.bam.bai ]; then
+	
+		# this should result in faster processing
+		echo "going to run samtools index $SAM.sorted.bam" >> $LOG
+		samtools index $SAM.sorted.bam 2>> $ERR
+	else
+		echo "BAM file index $SAM.sorted.bam.bai already created" >> $LOG
+	fi
+
+	# grow the list, will merge these in the end
+	BAMS="$BAMS $SAM.sorted.bam"
+done
+
+# now merge all the individuals
+BAM=$RESULTS/$SPECIES-merged.bam
+if [ ! -e $BAM ]; then
+	echo "going to create merged BAM file $BAM" >> $LOG
+	samtools merge $BAM $BAMS 2>> $ERR
+else
+	echo "merged BAM file $BAM already created" >> $LOG
+fi
